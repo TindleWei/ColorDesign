2de66a0d16ecd2a4b87f49e2aecf9e522cedf933
TindleWei <tindle.wei@gmail.com>
2015-02-10 14:42:18 +0800

    color design initial
    first 4 color buttons

---
+++ app/src/main/java/com/menkey/colordesign/ColorButtonActivity.java
@@ -0,0 +1,17 @@
+package com.menkey.colordesign;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by mac on 15/2/10.
+ */
+public class ColorButtonActivity extends FragmentActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_blank);
+
+    }
+}
