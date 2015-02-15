028abd13bb712912d8f807b0cb997564c1f5df3b
TindleWei <tindle.wei@gmail.com>
2015-02-15 17:56:25 +0800

    DynamicLayout 2
    1. RelativeLayout()
    2. generateViewId()
    3. view.setId()

---
+++ app/src/main/java/com/menkey/colordesign/DynamicLayout2Activity.java
@@ -0,0 +1,96 @@
+package com.menkey.colordesign;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.TypedValue;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by mac on 15/2/15.
+ */
+public class DynamicLayout2Activity extends FragmentActivity {
+
+    Context mContext = null;
+
+
+    /**
+     * Converting Density Independent Pixels (dp) to Pixels (px)
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mContext = this;
+
+        Button myButton = new Button(this);
+        myButton.setText("Press me");
+
+        EditText myEditText = new EditText(this);
+
+
+        myButton.setId(generateViewId());
+        myEditText.setId(generateViewId());
+
+        RelativeLayout myLayout = new RelativeLayout(this);
+        myLayout.setBackgroundColor(Color.BLUE);
+
+        RelativeLayout.LayoutParams buttonParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonParams.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        RelativeLayout.LayoutParams textParams =
+                new RelativeLayout.LayoutParams(
+                        RelativeLayout.LayoutParams.WRAP_CONTENT,
+                        RelativeLayout.LayoutParams.WRAP_CONTENT);
+
+        textParams.addRule(RelativeLayout.ABOVE, myButton.getId());
+        textParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        textParams.setMargins(0, 0, 0, 80);
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_DIP, 200, r.getDisplayMetrics());
+
+        myEditText.setWidth(px);
+
+        myLayout.addView(myButton, buttonParams);
+        myLayout.addView(myEditText, textParams);
+        setContentView(myLayout);
+    }
+
+    public void genViewId(View view) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            view.setId(generateViewId());
+        } else {
+            view.setId(View.generateViewId());
+        }
+    }
+
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    public static int generateViewId() {
+        for (; ; ) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+
+}
